288b788f4e399b45337808361234622658334953
Stephen Martinez <stephen.mtz.contact@gmail.com>
2026-08-02 23:53:00 -0700

    update to cmake

--- firmware/drivers/mosfet.h
+++ firmware/drivers/mosfet.h
@@ -1,10 +1,22 @@
 #pragma once 
+#include <cstdint>
 
+extern "C" { 
+    #include "stm32f4xx_hal.h"
+}
 namespace drivers { 
     class Mosfet { 
         public: 
+            Mosfet(TIM_HandleTypeDef *htim, uint32_t channel); 
+
             bool init(); 
             bool setDutyCycle(float pct);
             bool off(); 
-    };
+            bool on(); 
+            bool isOn() const; 
+        private: 
+            TIM_HandleTypeDef *htim_; 
+            uint32_t channel_; 
+            float dutyPercent_; 
+    };  
 }
